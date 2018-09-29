fb3f0e67af0e4adb93f79cfce9d64ec24f317aef
e <yjx2323999451>
2018-09-29 15:36:37 +0800

    增加拦截器的功能interceptro

---
+++ src/main/java/com/example/config/WebAppConfig.java
@@ -0,0 +1,25 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+import com.example.handle.LoginInterCeptor;
+
+
+
+@Configuration
+public class WebAppConfig  extends  WebMvcConfigurationSupport{
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+	
+		registry.addInterceptor(new LoginInterCeptor()).addPathPatterns("\"/**\"");
+		
+		
+	}
+	    
+
+}
---
+++ src/main/java/com/example/handle/LoginInterCeptor.java
@@ -0,0 +1,45 @@
+package com.example.handle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterCeptor  implements   HandlerInterceptor {
+	
+	
+	
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("------------------"+"拦截之前的操作"+"------------------------");
+	//	return HandlerInterceptor.super.preHandle(request, response, handler);
+		
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("------------------"+"处理请求之后再返回mdoleAndView之前的操作"+"------------------------");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		//HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+		
+		System.out.println("------------------"+"处理完视图之后的操作"+"------------------------");
+	}
+	 
+	 
+	 
+	 
+
+}
